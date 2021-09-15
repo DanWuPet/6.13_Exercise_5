91f39bfbbcf701dc9a6f68d76ae52bbf403f59bc
DanWuPet <danx1698@edu.easj.dk>
2021-09-15 13:38:41 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void isDoubloon(String input) {
+        int count = 0;
+        int index = input.length() - 1;
+        for (int i = 0; i < index; i++) {
+            for (int j = i + 1; j < input.length(); j++) {
+
+                if (input.charAt(i) == input.charAt(j)) {
+                    count++;
+                }
+            }
+        }
+        if (count != 0 && count % 2 == 0) {
+            System.out.println("String is Doubloon ");
+        } else {
+            System.out.println("String is not Doubloon ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the character :");
+        String input = scan.nextLine().toLowerCase();
+        scan.close();
+        isDoubloon(input);
+    }
+}
